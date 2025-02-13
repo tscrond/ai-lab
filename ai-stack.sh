90c5160da56df0496b99a0a121b8a002c1a102ad
Tomasz Skrond <tomasz.skrond@boar.network>
2025-02-13 21:33:29 +0000

    use https://github.com/AbdBarho/stable-diffusion-webui-docker/tree/master resources in the repo

---
+++ ai-stack.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+help() { ## Show help
+  echo "Usage: $0 <command>"
+  grep -E '^[a-zA-Z0-9_\-]+ *\(\) *{ *##' "$0" \
+    | sed 's/() *{ *## */ - /g' \
+    | awk '{printf "\t%-30s %s\n", $1, substr($0, index($0, $3))}'
+}
+
+download() { ## Downloads stable diffusion models
+  docker compose run --build download 
+}
+
+run() { ## Run whole stack
+  docker compose --profile ${1:-"auto"} up -d --build --remove-orphans
+}
+
+if [ $# -eq 0 ]; then
+  help
+  exit 1
+else
+  "$@"
+fi
